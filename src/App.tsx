e306b59615e1016a7e375a72ef5e3d19e4dc9b7b
hyeri-woo <woohyeri0525@gmail.com>
2023-08-25 01:10:30 +0900

    Feat: Home 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import Layout from 'components/common/Layout';
+import Home from 'pages/Home';
 import Signin from 'pages/SignIn';
 import Signup from 'pages/Signup';
 import Todo from 'pages/Todo';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import GlobalStyle from 'styles/GlobalStyle';
 
 function App() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path='/' element={<h1>Home </h1>} />
-          <Route path='/todo' element={<Todo />} />
-          <Route path='/signin' element={<Signin />} />
-          <Route path='/signup' element={<Signup />} />
-        </Routes>
-      </Layout>
+      <GlobalStyle />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/todo' element={<Todo />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
     </BrowserRouter>
   );
 }
