a5e82c3bd4aa8045f844edeac5b3b72aa32ff1be
hyeri-woo <woohyeri0525@gmail.com>
2023-08-25 13:49:59 +0900

    Feat: NotFound 구현 완료

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,49 @@
+import Button from 'components/common/Button';
+import { AuthContext } from 'context/auth/AuthContext';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { ContainerStyle, HeaderStyle } from 'styles/CommonStyle';
+
+const MainStyle = styled.main`
+  padding: 30px;
+  text-align: center;
+  button {
+    margin-top: 30px;
+  }
+  h2 {
+    font-size: 40px;
+    margin-bottom: 30px;
+  }
+`;
+
 export default function NotFound() {
-  return <div>NotFound</div>;
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+  return (
+    <ContainerStyle>
+      <HeaderStyle>
+        <h1>Not Found</h1>
+      </HeaderStyle>
+      <MainStyle>
+        <h2>404</h2>
+        <p>접근할 수 없는 페이지입니다.</p>
+        {user ? (
+          <Button
+            type='button'
+            dataTestId=''
+            text='로그인 페이지로 이동'
+            onClick={() => navigate('/signin')}
+          />
+        ) : (
+          <Button
+            type='button'
+            dataTestId=''
+            text='일정 페이지로 이동'
+            onClick={() => navigate('/todo')}
+          />
+        )}
+      </MainStyle>
+    </ContainerStyle>
+  );
 }
